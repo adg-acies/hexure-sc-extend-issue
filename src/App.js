98190d757784849ef605e83c2a0241513b86c8a9
Amandeep Gupta <amandeep.gupta@aciesglobal.com>
2023-09-06 21:46:57 +0530

    foo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import "./App.css";
 import { Button } from "@hexure/ui";
+import { Foo } from "./Foo";
 
 const SButton = styled(Button)`
   background: red;
@@ -9,6 +10,7 @@ const SButton = styled(Button)`
 function App() {
   return (
     <div className="App">
+      <Foo />
       <Button>Hexure button</Button>
       <SButton>Extended Hexure button</SButton>
     </div>
